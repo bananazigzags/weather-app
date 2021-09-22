30173cd953e8022db09c646c79796ae3de1953e2
bananazigzags <bananazigzags@gmail.com>
2021-09-22 23:33:29 +0300

    added other data points

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const temp = document.getElementById("temp");
+const feels = document.getElementById("feels-like");
+const tempMin = document.getElementById("temp-min");
+const tempMax = document.getElementById("temp-max");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
 
-function getTemp(city) {
+function getWeather(city) {
    
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=cf317dc7c3d35c6dc81c100d587453a4`, {mode: 'cors'})
     .then(function(response) {
         return response.json();
     })
     .then(function(response) {
-        temp.textContent = `${response.main.temp}\u00B0`;
+        temp.textContent = `Current: ${response.main.temp}\u00B0`;
+        feels.textContent = `Feels Like: ${response.main.feels_like}\u00B0`;
+        tempMin.textContent = `Minimum Today: ${response.main.temp_min}\u00B0`;
+        tempMax.textContent = `Maximum Today: ${response.main.temp_max}\u00B0`;
+        humidity.textContent = `Humidity: ${response.main.humidity}%`;
+        wind.textContent = `Wind: ${response.wind.speed} km/h`;
+
         console.log(response);
     });
 }
@@ -17,6 +28,6 @@ const input = document.querySelector("input");
 form.addEventListener('keydown', (event) => {
     if (event.code === 'Enter') {
         event.preventDefault();
-        getTemp(input.value);
+        getWeather(input.value);
     }
 })
